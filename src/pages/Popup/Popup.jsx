dabad322cfb78d811d0b0f6d54e18443d2bbd48c
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2023-08-19 14:38:34 -0400

    feat: Add basic profile page

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,25 +1,62 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
+import profileIcon from '../../assets/img/profileIcon.png';
+import verified from '../../assets/img/verified.png';
+
+export function shortenAddress(address) {
+  if (address.length < 10) {
+    return address; // Address is too short to shorten
+  }
+  const firstPart = address.slice(0, 6);
+  const lastPart = address.slice(-3);
+  return `${firstPart}...${lastPart}`;
+}
+
+const TwitterContainer = ({ platform, handle }) => {
+  return (
+    <div className='twitter-container'>
+      <p>
+        {platform}: {handle}
+      </p>
+    </div>
+  );
+};
 
 const Popup = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='popup-container'>
+      <div className='profile-header'>
+        <h1 className='profile-name'>
+          John Doe
+          <img className='verified-icon' src={verified} alt='Profile Icon' />
+        </h1>
+      </div>
+      <div className='profile-container'>
         <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
+          <a href='https://etherscan.io/address/0xe94f1fa4f27d9d288ffea234bb62e1fbc086ca0c'>
+            {' '}
+            {shortenAddress('0xE94f1fa4F27D9d288FFeA234bB62E1fBC086CA0c')}
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+        <div className='profile-icon'>
+          <img src={profileIcon} alt='Profile Icon' />
+        </div>
+        <div className='profile-details'>
+          <p>
+            <strong>About</strong>: Web3 Developer @ FuturistConf
+          </p>
+          <p>
+            <strong>Website</strong>:{' '}
+            <a href='https://www.example.com'>https://www.example.com</a>
+          </p>
+        </div>
+          <div className='grid-container'>
+            <TwitterContainer platform='facebook' handle='@abc' />
+            <TwitterContainer platform='twitter' handle='@def' />
+            <TwitterContainer platform='linkedin' handle='@ghi' />
+            <TwitterContainer platform='instagram' handle='@jkl' />
+          </div>
+        </div>
     </div>
   );
 };
