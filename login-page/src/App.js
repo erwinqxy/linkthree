e64293fe98c728f0d81be18dee51f407062be786
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2023-08-21 00:07:47 -0400

    feat: Add metamusk login

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -1,23 +1,95 @@
-// App.js
 import './App.css';
-import {
-  FacebookLoginButton,
-  InstagramLoginButton,
-  LinkedInLoginButton,
-  TwitterLoginButton,
-} from 'react-social-login-buttons';
+import { MetamaskLoginButton } from 'react-social-login-buttons';
 import icon from './icon.png';
 import Popup from './Popup';
+import { useState, useEffect } from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
+
+import { formatBalance, formatChainAsNum } from './utils';
+
+export function shortenAddress(address) {
+  if (address.length < 10) {
+    return address; // Address is too short to shorten
+  }
+  const firstPart = address.slice(0, 8);
+  const lastPart = address.slice(-4);
+  return `${firstPart}...${lastPart}`;
+}
 
 function App() {
-  function handleLoginClick() {
-    alert('Login button clicked!');
-  }
+  const [hasProvider, setHasProvider] = useState(null);
+  const initialState = { accounts: [], balance: '', chainId: '' };
+  const [wallet, setWallet] = useState(initialState);
 
-  function handleConnectWalletClick() {
-    alert('Connect Wallet button clicked!');
-    // You can implement wallet connection logic here
-  }
+  const [isConnecting, setIsConnecting] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    const refreshAccounts = (accounts) => {
+      if (accounts.length > 0) {
+        updateWallet(accounts);
+      } else {
+        // if length 0, user is disconnected
+        setWallet(initialState);
+      }
+    };
+
+    const refreshChain = (chainId) => {
+      setWallet((wallet) => ({ ...wallet, chainId }));
+    };
+
+    const getProvider = async () => {
+      const provider = await detectEthereumProvider({ silent: true });
+      setHasProvider(Boolean(provider));
+
+      if (provider) {
+        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+        refreshAccounts(accounts);
+        window.ethereum.on('accountsChanged', refreshAccounts);
+        window.ethereum.on('chainChanged', refreshChain);
+      }
+    };
+
+    getProvider();
+
+    return () => {
+      window.ethereum?.removeListener('accountsChanged', refreshAccounts);
+      window.ethereum?.removeListener('chainChanged', refreshChain);
+    };
+  }, []);
+
+  const updateWallet = async (accounts) => {
+    const balance = formatBalance(
+      await window.ethereum.request({
+        method: 'eth_getBalance',
+        params: [accounts[0], 'latest'],
+      })
+    );
+    const chainId = await window.ethereum.request({
+      method: 'eth_chainId',
+    });
+    setWallet({ accounts, balance, chainId });
+  };
+
+  const handleConnect = async () => {
+    setIsConnecting(true);
+    await window.ethereum
+      .request({
+        method: 'eth_requestAccounts',
+      })
+      .then((accounts) => {
+        setError(false);
+        updateWallet(accounts);
+      })
+      .catch((err) => {
+        setError(true);
+        setErrorMessage(err.message);
+      });
+    setIsConnecting(false);
+  };
+
+  const disableConnect = Boolean(wallet) && isConnecting;
 
   return (
     <div className='App'>
@@ -27,12 +99,27 @@ function App() {
             <img src={icon} alt='LinkThree Logo' className='company-logo' />
             <span className='company-name'>LinkThree</span>
           </div>
-          <button className='connect-wallet-button' onClick={handleConnectWalletClick}>
-            Connect Wallet
-          </button>
+          <div className='metamask-button-container'>
+            {window.ethereum?.isMetaMask && wallet.accounts.length < 1 && (
+              <MetamaskLoginButton disabled={disableConnect} onClick={handleConnect} />
+            )}
+            {wallet.accounts.length > 0 && (
+              <div className='wallet-accounts-container'>
+                <div className='wallet-accounts'>
+                  <p>Wallet: {shortenAddress(wallet.accounts[0])} connected 🟢</p>
+                </div>
+              </div>
+            )}
+            {error && (
+              <div onClick={() => setError(false)}>
+                <strong>Error:</strong> {errorMessage}
+              </div>
+            )}
+          </div>
         </div>
+
         <div className='login-box'>
-          <Popup></Popup>
+          <Popup />
         </div>
       </header>
     </div>
